be854a790510af8e8c6bd03fc3b2f05b394cb7b4
Denys800 <casssio@yahoo.com>
2025-02-20 15:51:24 +0200

    homework-section 2

---
+++ section-2/exercises/dKozlovskyi.js
@@ -0,0 +1,43 @@
+//Exercise 1:
+let currentTemperature;
+let isRaining = false;
+let cityName;
+
+//Exercise 3:
+let messageOne = 'some random text'
+let messageTwo = messageOne;
+console.log(messageOne)
+console.log(messageTwo)
+
+//Exercise 4:
+//Output "Igor"
+
+//Exercise 5:
+const text = "Hello World!"
+console.log(text.toLowerCase());
+console.log(text.includes('Hello'));
+console.log(text.substring(6, 11));
+
+//Exercise 6:
+
+let name = "John"
+let age = 25
+console.log(`Hello, my name is ${name} and I'm ${age} years old`)
+
+// Exercise 7:
+// What will be the result of each console log ? Think without code execution.
+//
+// console.log(5 == "5"); - true
+// console.log(5 === "5"); - false
+// console.log(!false); - true
+
+// Exercise 8:
+// What will show console log in the code bellow ? Think without code execution.
+//
+//     const person = { name: "Alice",
+//                      age: 25 }.
+//     const personTwo = person;
+// personTwo.age = 33;
+//
+// console.log(personTwo.age); 33
+// console.log(person.age); 33
