6e22cc1669ca9427eb09500c2f5d36dc1fbe4b0e
o.sidorov <oleksandr.sidorov.ua@gmail.com>
2025-03-20 14:23:56 +0200

    OS_home_exercieses_section_2

---
+++ section-2/exercises/OSidorov_Sec_2.js
@@ -0,0 +1,46 @@
+//Exercise 1:
+const currentTemperature = 36;
+const isRaining = true;
+const cityName = 'Kyiv';
+
+//Exercise 2:
+Test
+
+//Exercise 3:
+let messageOne;
+messageOne = 'a';
+let messageTwo;
+messageTwo = 'a';
+console.log(messageOne + ' ' + messageTwo);
+
+//Exercise 4:
+Igor
+
+//Exercise 5:
+//1.
+const text = "Hello World!"
+console.log(text.toLowerCase());
+//2.
+console.log(text.includes('Hello'));
+//3.
+console.log(text.substring(6, 11));
+
+//Exercise 6:
+const name = 'John';
+const age = 25;
+console.log(`Hello, my name is ${name} and I'm ${age} years old`);
+
+//Exercise 7:
+//console.log(5 == "5");
+true
+//console.log(5 === "5");
+false
+//console.log(!false);
+true
+
+//Exercise 8:
+// console.log(personTwo.age); ??
+33
+// console.log(person.age); ??
+33
+//cause of HEAP behavior
