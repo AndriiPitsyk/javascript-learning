99d721d02c7eedeb4c3393cd5e3d13a6d0d7d2a3
o.sidorov <oleksandr.sidorov.ua@gmail.com>
2025-06-26 18:10:37 +0300

    OS_home_exercieses_section_2

--- section-2/exercises/OSidorov_Sec_2.js
+++ section-2/exercises/OSidorov_Sec_2.js
@@ -3,10 +3,8 @@ const currentTemperature = 36;
 const isRaining = true;
 const cityName = 'Kyiv';
 
-
 //Exercise 2:
 Test
-
 
 //Exercise 3:
 let messageOne;
@@ -15,10 +13,8 @@ let messageTwo;
 messageTwo = 'a';
 console.log(messageOne + ' ' + messageTwo);
 
-
 //Exercise 4:
 Igor
-
 
 //Exercise 5:
 //1.
@@ -29,12 +25,10 @@ console.log(text.includes('Hello'));
 //3.
 console.log(text.substring(6, 11));
 
-
 //Exercise 6:
 const name = 'John';
 const age = 25;
 console.log(`Hello, my name is ${name} and I'm ${age} years old`);
-
 
 //Exercise 7:
 //console.log(5 == "5");
@@ -43,7 +37,6 @@ true
 false
 //console.log(!false);
 true
-
 
 //Exercise 8:
 // console.log(personTwo.age); ??
